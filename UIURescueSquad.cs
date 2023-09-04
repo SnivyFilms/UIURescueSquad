caac2fce64827ae5da49c7bce9ff8b6a94465153
Nikki Gardiner <mill385@yahoo.com>
2023-09-04 14:01:27 -0400

    Small (I do mean it) changes

--- UIURescueSquad.cs
+++ UIURescueSquad.cs
@@ -12,10 +12,10 @@ namespace UIURescueSquad
     {
 
         public override string Name { get; } = "UIURescueSquad";
-        public override string Author { get; } = "JesusQC, Michal78900, maintained by Marco15453";
+        public override string Author { get; } = "JesusQC, Michal78900, maintained by Marco15453. Updated to Exiled 8 by Vicious Vikki";
         public override string Prefix { get; } = "UIURescueSquad";
         public override Version Version { get; } = new Version(5, 3, 0);
-        public override Version RequiredExiledVersion => new Version(7, 1, 0);
+        public override Version RequiredExiledVersion => new Version(8, 1, 0);
 
         public bool IsSpawnable = false;
 
