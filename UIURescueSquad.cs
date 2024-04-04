5fcd7cc2a1c706d238e92d437afd9dc6f54f116e
Snivy FIlms <120346554+NikkiGardiner1@users.noreply.github.com>
2024-04-04 09:41:55 -0400

    Pump Proj Version and Required Exiled Version

--- UIURescueSquad.cs
+++ UIURescueSquad.cs
@@ -14,8 +14,8 @@ namespace UIURescueSquad
           public override string Name { get; } = "UIURescueSquad";
           public override string Author { get; } = "JesusQC, Michal78900, Marco15453, Vicious Vikki, & Misfiy";
           public override string Prefix { get; } = "UIURescueSquad";
-          public override Version Version { get; } = new Version(5, 3, 1);
-          public override Version RequiredExiledVersion => new Version(8, 2, 1);
+          public override Version Version { get; } = new Version(5, 3, 2);
+          public override Version RequiredExiledVersion => new Version(8, 8, 0);
           
           public static UIURescueSquad Instance;
 
